d0adf6dfdaa5da6bb5a8342977aac4e90945a6e1
DevJahanzaibJaved <Devjahanzaibjaved84@gmail.com>
2023-06-21 22:24:39 +0500

    fix in image record migration

--- db/migrate/20230619113049_create_image_record.rb
+++ db/migrate/20230619113049_create_image_record.rb
@@ -1,7 +1,7 @@
 class CreateImageRecord < ActiveRecord::Migration[7.0]
   def change
-    create_table :image_records, id: :uuid do |t|
-      t.references :session, null: true, foreign_key: true, type: :uuid
+    create_table :image_records do |t|
+      t.references :session, null: true, foreign_key: true
       t.string :image_url
 
       t.timestamps
